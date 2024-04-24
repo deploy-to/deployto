896f1ea184da4951b123f709157e5c857988ae13
Dmitry Goncharov <dmitry.goncharov@mts.ru>
2024-04-25 00:47:00 +0300

    complete job

--- src/cmd/job.go
+++ src/cmd/job.go
@@ -1,11 +1,13 @@
 package cmd
 
 import (
+	"bytes"
 	"context"
 	"deployto/src/types"
 	"deployto/src/yaml"
 	"errors"
 	"fmt"
+	"io"
 	"os"
 	"os/exec"
 	"strings"
@@ -15,9 +17,54 @@ import (
 	"github.com/urfave/cli/v2"
 )
 
-const (
-	citimeout = "10" //munutes
-)
+type WriterWithReadFrom interface {
+	io.Writer
+	io.ReaderFrom
+}
+
+type ContextWrappedWriter struct {
+	w WriterWithReadFrom
+	c context.Context
+}
+
+type ReadFromResult struct {
+	n   int64
+	err error
+}
+
+func (cww *ContextWrappedWriter) Write(p []byte) (n int, err error) {
+	return cww.Write(p)
+}
+
+func (cww *ContextWrappedWriter) ReadFrom(r io.Reader) (n int64, err error) {
+	if c, ok := r.(io.Closer); ok {
+		ch := make(chan ReadFromResult, 1)
+		go func() {
+			n, err := cww.w.ReadFrom(r)
+			ch <- ReadFromResult{n, err}
+		}()
+
+		closed := false
+		for {
+			select {
+			case res := <-ch:
+				return res.n, res.err
+			case <-cww.c.Done():
+				if !closed {
+					closed = true
+					err := c.Close()
+					if err != nil {
+						return 0, fmt.Errorf("error closing reader: %v", err)
+					}
+				}
+				time.Sleep(time.Second * 1)
+			}
+		}
+
+	} else {
+		return cww.w.ReadFrom(r)
+	}
+}
 
 func callJob(cCtx *cli.Context) error {
 	path, err := os.Getwd()
@@ -32,17 +79,29 @@ func callJob(cCtx *cli.Context) error {
 		log.Error().Int("len(app)", len(apps)).Str("path", path).Msg("wait one app")
 		return errors.New("APP NOT FOUND")
 	}
-
-	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(1)*time.Millisecond)
+	cCtx.Args()
+	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(timeout)*time.Minute)
 	defer cancel()
 	for _, s := range apps {
 		for _, c := range s.Spec.Steps {
 			stringSlice := strings.Split(c.Run, "\n")
 			for _, cc := range stringSlice {
-				if err := exec.CommandContext(ctx, cc).Run(); err != nil {
-					timeoutMsg := "Ci Stoped, timeout" + cCtx.String("citimeout")
-					log.Error().Err(err).Msg(timeoutMsg)
+				if len(cc) > 0 {
+					ccc := strings.Split(cc, " ")
+					var Stdout, Stderr bytes.Buffer
+
+					c := exec.CommandContext(ctx, ccc[0], ccc[1:]...)
+					c.Stderr = &ContextWrappedWriter{&Stderr, ctx}
+					c.Stdout = &ContextWrappedWriter{&Stdout, ctx}
+					err := c.Run()
+					if err != nil {
+						timeoutMsg := "Ci Stoped, timeout" + cCtx.String("citimeout")
+						log.Error().Err(err).Msg(timeoutMsg)
+					}
+					fmt.Println(Stderr.String())
+					fmt.Println(Stdout.String())
 				}
+
 			}
 
 		}
@@ -51,6 +110,8 @@ func callJob(cCtx *cli.Context) error {
 
 	return nil
 }
+
+var timeout int
 
 var Job = &cli.Command{
 	Name:    "job",
@@ -61,24 +122,25 @@ var Job = &cli.Command{
 			Name:  "run",
 			Usage: "run a job",
 			Action: func(cCtx *cli.Context) error {
-				fmt.Println("new task template: ", cCtx.Args().First())
+				fmt.Println("new Job Run: ", cCtx.Args().First())
 				callJob(cCtx)
 				return nil
 			},
-		},
-	},
-	Flags: []cli.Flag{
-		&cli.IntFlag{
-			Name:        "timeout",
-			Aliases:     []string{"t"},
-			Value:       10, //minutes
-			Usage:       "timeout for ci process",
-			DefaultText: "10 minutes",
-			Action: func(ctx *cli.Context, v int) error {
-				if v >= 100 {
-					return fmt.Errorf("Flag timeout value %v out of range[1-100]", v)
-				}
-				return nil
+			Flags: []cli.Flag{
+				&cli.IntFlag{
+					Name:        "timeout",
+					Aliases:     []string{"t"},
+					Value:       10, //minutes
+					Usage:       "timeout for ci process",
+					DefaultText: "10 minutes",
+					Action: func(ctx *cli.Context, v int) error {
+						if v >= 100 {
+							return fmt.Errorf("Flag timeout value %v out of range[1-100]", v)
+						}
+						return nil
+					},
+					Destination: &timeout,
+				},
 			},
 		},
 	},
