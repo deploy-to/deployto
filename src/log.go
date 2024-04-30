d46368a7279c6c74ef68802cbdd73b904513d803
poncheg <pon.otec@gmail.com>
2024-05-01 00:37:09 +0300

    Adding logging feature (#5)
    * done
    
    * fix
    
    * fix

--- src/log.go
+++ src/log.go
@@ -1,20 +1,42 @@
 package src
 
 import (
+	"deployto/src/types"
 	"os"
+	"time"
 
 	"github.com/k0kubun/pp/v3"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 )
 
-func LogSetting() {
-	//TODO set level from flag
-	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
-	zerolog.SetGlobalLevel(zerolog.DebugLevel)
+func LogSetting(stg types.Settings) {
+	//trace, debug, warn, info, fatal, panic, absent, disable
+	switch stg.Loglevel {
+	case "trace":
+		zerolog.SetGlobalLevel(zerolog.TraceLevel)
+	case "debug":
+		zerolog.SetGlobalLevel(zerolog.DebugLevel)
+	case "warn":
+		zerolog.SetGlobalLevel(zerolog.WarnLevel)
+	case "info":
+		zerolog.SetGlobalLevel(zerolog.InfoLevel)
+	case "fatal":
+		zerolog.SetGlobalLevel(zerolog.InfoLevel)
+	case "panic":
+		zerolog.SetGlobalLevel(zerolog.PanicLevel)
+	case "absent":
+		zerolog.SetGlobalLevel(zerolog.NoLevel)
+	case "disable":
+		zerolog.SetGlobalLevel(zerolog.Disabled)
+	}
 
-	//	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+	switch stg.Logformat {
+	case "pretty":
+		log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+	}
 
-	log.Debug().Msg("zerolog.DebugLevel")
+	zerolog.TimeFieldFormat = time.DateTime
+
 	pp.Default.SetColoringEnabled(false)
 }
---
+++ src/types/settings.go
@@ -0,0 +1,6 @@
+package types
+
+type Settings struct {
+	Loglevel  string
+	Logformat string
+}
