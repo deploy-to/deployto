dc52f9d95c75ae205c1acbd94c5dec57d75b5005
Dmitry G <pon.otec@gmail.com>
2024-05-01 13:50:29 +0300

    fix find git tag

--- src/gitclient/getvalues.go
+++ src/gitclient/getvalues.go
@@ -4,8 +4,10 @@ import (
 	"deployto/src/helper"
 	"deployto/src/types"
 	"path/filepath"
+	"sort"
 
 	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing"
 	"github.com/lithammer/shortuuid/v3"
 	"github.com/rs/zerolog/log"
 )
@@ -52,12 +54,24 @@ func GetValues(path string) (values types.Values) {
 	values["CommitShort"] = ref.Hash().String()[:7] + dirtyPostfix
 
 	//Find tag (semver or another one)
-	tag, err := rep.TagObject(ref.Hash())
+	tagrefs, err := rep.Tags()
 	if err != nil {
 		log.Debug().Err(err).Msg("Git tag not found")
 	}
-	if tag != nil {
-		values["Tag"] = tag.Name + dirtyPostfix
+	var Tags []string
+	err = tagrefs.ForEach(func(t *plumbing.Reference) error {
+		if t.Hash().String() == ref.Hash().String() {
+			Tags = append(Tags, t.Name().Short())
+		}
+		return nil
+	})
+	if err != nil {
+		log.Debug().Err(err).Msg("Git tag not found")
+	}
+
+	if len(Tags) > 0 {
+		sort.Strings(Tags)
+		values["Tag"] = Tags[len(Tags)-1] + dirtyPostfix
 	}
 
 	log.Debug().Any("values", values).Msg("gitclient.GetValues")
