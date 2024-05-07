ec852350609fc5025d975bd4e42398038bdcafa7
Dmitry Goncharov <123@123.ru>
2024-05-08 01:55:45 +0300

    fix ci

--- examples/terraform-yandex/provider.tf
+++ examples/terraform-yandex/provider.tf
@@ -25,7 +25,7 @@ terraform {
   required_version = ">= 0.13"
   required_providers {
     yandex = {
-      source = "yandex-cloud/yandex"
+      source = "terraform-mirror.yandexcloud.net/yandex-cloud/yandex"
     }
   }
 }
