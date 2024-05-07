5b85f2e1eca5b22935e3cf7671881e248a6fadec
Dmitry Goncharov <123@123.ru>
2024-05-08 01:32:30 +0300

    fix path to folder

--- src/deploy/terraform_test.go
+++ src/deploy/terraform_test.go
@@ -7,14 +7,14 @@ import (
 
 func TestTerraform(t *testing.T) {
 	// check Hello World from Terraform
-	path := "/Users/myoffice/Documents/deployto/examples/terraform-hello-world/"
+	path := "../../examples/terraform-hello-world/"
 	output, err := Terraform(nil, nil, path, []string{"AAAAA"}, types.Values(nil), types.Values(nil))
 	if err != nil {
 		t.Fatalf("Terraform error %v", err)
 	}
 	t.Logf("Terraform output is = %v", output)
 	// terraform yandex check
-	path2 := "/Users/myoffice/Documents/deployto/examples/terraform-yandex"
+	path2 := "../../examples/terraform-yandex"
 	output, err = Terraform(nil, nil, path2, []string{"AAAAA"}, types.Values(nil), types.Values(nil))
 	if err != nil {
 		t.Fatalf("Terraform error %v", err)
